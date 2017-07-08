eba7a59a1240a956886849b110c6021fac1c2786
vintisachdeva <vintisachdeva17@gmail.com>
2017-07-08 20:29:25 +0530

    Add files via upload

---
+++ 8 July--Video View and Food Lover/video view/videoview/MainActivity.java
@@ -0,0 +1,39 @@
+package com.bmpl.videoview;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    VideoView videoView;
+
+    String pathOfVideo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        videoView = (VideoView)findViewById(R.id.videoView);
+
+        pathOfVideo = "android.resource://" + getPackageName() + "/" + R.raw.small;
+
+        videoView.setVideoPath(pathOfVideo);
+
+        videoView.setMediaController(new MediaController(this));
+
+        //videoView.start();
+
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+
+                videoView.start();
+            }
+        });
+
+    }
+}
